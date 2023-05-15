cd8c3b33c9a5c013d8b37462fd034ba39e749087
Arun Chaitanya Jami <arun.chaitanyajami@news.co.uk>
2023-05-16 00:46:24 +0530

    feat(rankmath): Base plugin with docker setup to run in vanilla wordpress

---
+++ rank-math-coding-challenge.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: RankMath Dashboard Widget ( Coding Challenge )
+ * Plugin URI: https://github.com/arunchaitanyajami/rank-math-coding-challenge-aj
+ * Description: A custom WordPress dashboard Widget that displays Recharts line chart with filters of last 7 days, 15 days, and 1 month.
+ * Author: arunchaitanyajami
+ * Author URI: https://github.com/arunchaitanyajami
+ * Text Domain: rankmath-dashboard-widget
+ * Version: 1.0.0
+ *
+ * @package WordPress
+ */
